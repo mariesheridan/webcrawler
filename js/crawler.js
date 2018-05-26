b07de99a72e959fef64cc1136cec560164116e27
unknown <marie.sheridan.tuazon@gmail.com>
2018-05-26 18:24:49 +0800

    Moved getLinks to a new method.

--- js/crawler.js
+++ js/crawler.js
@@ -2,29 +2,24 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
 
+var visitedLinks = [];
+var json = {
+    hrefs: [],
+    links :[],
+    scripts: []
+};
+
 module.exports = {
     crawl: function (url, depth, includeAssets, callback) {
-        request(url, function(error, response, html){
-            console.log('request callback');
-            var json = {
-                links:[]
-            };
 
-            if(!error) {
-                var $ = cheerio.load(html);
+        var iteration = 1;
 
-                $('a').each(function(){
-                    var href = $(this).attr('href');
-                    json.links.push(href);
-                });
-            } else {
-                console.log('Error: ' + error);
-            }
+        getLinks(url, includeAssets, function(links) {
 
             fs.writeFile(
                 'output.json',
                 JSON.stringify(json, null, 4),
-                function(err){
+                function(err) {
                     console.log('File successfully written! - Check your project directory for the output.json file');
                     callback();
                 }
@@ -32,3 +27,35 @@ module.exports = {
         });
     }
 };
+
+var getLinks = function(url, includeAssets, callback) {
+
+    request(url, function(error, response, html) {
+
+        console.log('request callback');
+
+        if(!error) {
+            var $ = cheerio.load(html);
+
+            $('a').each(function(){
+                var href = $(this).attr('href');
+                json.hrefs.push(href);
+            });
+
+            $('script').each(function(){
+                var script = $(this).attr('src');
+                var script = $(this).html();
+                json.scripts.push(script);
+            });
+
+            $('link').each(function(){
+                var link = $(this).attr('href');
+                json.links.push(link);
+            });
+        } else {
+            console.log('Error: ' + error);
+        }
+
+        callback();
+    });
+}
