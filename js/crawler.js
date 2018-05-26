5ab27e9ac55c744b139706580c05aea8470fb364
unknown <marie.sheridan.tuazon@gmail.com>
2018-05-26 20:02:48 +0800

    Iterate 2 levels.

--- js/crawler.js
+++ js/crawler.js
@@ -12,13 +12,13 @@ var json = {
 module.exports = {
     crawl: function (url, depth, includeAssets, callback) {
 
-        var iteration = 1;
+        var level = 1;
 
-        getLinks(url, includeAssets, function(links) {
+        visitURLs([url], level, depth, includeAssets, function(urlList) {
 
             fs.writeFile(
                 'output.json',
-                JSON.stringify(json, null, 4),
+                JSON.stringify(urlList, null, 4),
                 function(err) {
                     console.log('File successfully written! - Check your project directory for the output.json file');
                     callback();
@@ -28,9 +28,25 @@ module.exports = {
     }
 };
 
-var getLinks = function(url, includeAssets, callback) {
+var visitURLs = function(urlList, level, depth, includeAssets, callback) {
 
-    request(url, function(error, response, html) {
+    console.log("Level " + level + " of " + depth);
+    var numOfURL = urlList.length;
+    var index = 0;
+
+    getLinks(urlList, index, includeAssets, function(newUrlList) {
+        if (level < depth) {
+            visitURLs(newUrlList, level + 1, depth, includeAssets, callback)
+        } else {
+            callback(newUrlList);
+        }
+    });
+}
+
+var getLinks = function(urlList, index, includeAssets, callback) {
+
+    request(urlList[index], function(error, response, html) {
+        var list = urlList;
 
         console.log('request callback');
 
@@ -39,23 +55,23 @@ var getLinks = function(url, includeAssets, callback) {
 
             $('a').each(function(){
                 var href = $(this).attr('href');
-                json.hrefs.push(href);
+                list.push(href);
             });
 
             $('script').each(function(){
                 var script = $(this).attr('src');
-                var script = $(this).html();
-                json.scripts.push(script);
+                // var script = $(this).html();
+                list.push(script);
             });
 
             $('link').each(function(){
                 var link = $(this).attr('href');
-                json.links.push(link);
+                list.push(link);
             });
         } else {
             console.log('Error: ' + error);
         }
 
-        callback();
+        callback(list);
     });
 }
